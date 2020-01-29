222d147622a33f27b4c9d31849393af22697af64
KseniyaGrekova <grekova_kseniya@inbox.com>
2020-01-29 10:39:56 +0300

    Проект магазина

---
+++ sh_project/shop/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import Category, Product
+import csv
+import datetime
+from django.http import HttpResponse
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name', 'slug']
+    prepopulated_fields={'slug':('name', )}
+
+def export_to_csv(modeladmin, request, queryset):
+    opts=modeladmin.model._meta
+    response=HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # whrite data rows
+    for obj in queryset:
+        data_row=[]
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    actions = [export_to_csv]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+
